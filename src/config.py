41832c8dd2bea1c8b917d70ad9538eca88c34c70
ssabrinahart <sabrinahart@vt.edu>
2025-07-29 17:12:05 -0400

    stash

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
+# In your config.py or app configuration
 import os
 
-
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "devsecretkey")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "lilly")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "postgresql://medijane_user:securepass@localhost:5432/medijane_db"
+        "postgresql://sabrina:lilly@localhost:5432/db_sabrina"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
